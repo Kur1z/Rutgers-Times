3158bb8875449cacad697d9d6617d19191a4d8c7
Josue Galvez <galvezjosue10@hotmail.com>
2023-01-31 16:25:38 -0500

    TESTING

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <body>
 
-<h1 class = title><img src="Resources/RU_SHIELD_RED.png"> RUTGERS TIMES</h1>
+<h1 class = title><img src="Resources/RU_SHIELD_RED.png"> RUTGERS TIMESSSSSSSS</h1>
 
 <div class="header">
   <h3>  <span id ='date'></span></h3>
