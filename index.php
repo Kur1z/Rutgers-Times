482393e3da955c75dcf14f3989f34bf5a42ebd78
Josue Galvez <galvezjosue10@hotmail.com>
2023-02-02 13:51:07 -0500

    Version 1 Index.php

--- index.php
+++ index.php
@@ -35,7 +35,6 @@ curl_close($curl);
 $data = json_decode($response, true);
 
 
-echo '<pre>'; print_r($data); echo '</pre>';
 
  
 ?>
@@ -72,33 +71,130 @@ echo '<pre>'; print_r($data); echo '</pre>';
 
 
 <div class="row">
-<div class= "rightColumn">
-    <h2><?php echo $data['articles'][0]["title"];?></h2>
-    <h5>Posted: <?php echo $data['articles'][0]["publishedAt"];?></h5>
-    <p><?php echo $data['articles'][0]["description"];?></p>
+<div class= "leftColumn">
+    <h2 id='headline'><a href = <?php echo $data['articles'][0]["url"] ?> ><?php echo $data['articles'][0]["title"];?> </a></h2>
+    <p id='description'><?php echo $data['articles'][0]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][0]["publishedAt"];?> by <?php echo $data['articles'][0]['source']['name'];?> </h5>
   </div>
   <div class = "middleColumn">
       
       <div class="leftImgs">
-    <?php  echo "<img  src='" . $data['articles'][0]["urlToImage"] . "' alt='" . $data['articles'][1]["title"]. "'>"; ?>
+      <?php  echo "<a href=" . $data['articles'][0]['url'] . " ><img  src='" . $data['articles'][0]['urlToImage'] . "' alt='" . $data['articles'][0]["title"]. "'> </a>"; ?>
       </div>
   </div>
 
-  <div class= "leftColumn">
-  <h2><?php echo $data['articles'][3]["title"];?></h2>
-    <h5><?php echo $data['articles'][3]["publishedAt"];?></h5>
+  <div class= "rightColumn">
+  <h2 id ='headline'> <a href = <?php echo $data['articles'][3]["url"] ?> ><?php echo $data['articles'][3]["title"];?> </a></h2>
+  
     <div class="rightImgs">
-    <?php  echo "<img  src='" . $data['articles'][3]["urlToImage"] . "' alt='" . $data['articles'][1]["title"]. "'>"; ?>
-      </div>
-    <p><?php echo $data['articles'][3]["description"];?></p>
+    <?php  echo "<a href=" . $data['articles'][3]['url'] . " ><img  src='" . $data['articles'][3]['urlToImage'] . "' alt='" . $data['articles'][3]["title"]. "'> </a>"; ?>
+    </div>
+    <p id= 'description'><?php echo $data['articles'][3]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][3]["publishedAt"];?> by <?php echo $data['articles'][3]['source']['name'];?> </h5>
   </div>
 </div>
 
 
+
+
+<div class="row">
+<div class= "leftColumn">
+    <h2 id='headline'><a href = <?php echo $data['articles'][1]["url"] ?> ><?php echo $data['articles'][1]["title"];?> </a></h2>
+    <p id='description'><?php echo $data['articles'][1]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][1]["publishedAt"];?> by <?php echo $data['articles'][1]['source']['name'];?> </h5>
+  </div>
+  <div class = "middleColumn">
+      
+      <div class="leftImgs">
+      <?php  echo "<a href=" . $data['articles'][1]['url'] . " ><img  src='" . $data['articles'][1]['urlToImage'] . "' alt='" . $data['articles'][1]["title"]. "'> </a>"; ?>
+      </div>
+  </div>
+
+  <div class= "rightColumn">
+  <h2 id ='headline'> <a href = <?php echo $data['articles'][2]["url"] ?> ><?php echo $data['articles'][2]["title"];?> </a></h2>
+  
+    <div class="rightImgs">
+    <?php  echo "<a href=" . $data['articles'][2]['url'] . " ><img  src='" . $data['articles'][2]['urlToImage'] . "' alt='" . $data['articles'][2]["title"]. "'> </a>"; ?>
+    </div>
+    <p id= 'description'><?php echo $data['articles'][2]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][2]["publishedAt"];?> by <?php echo $data['articles'][2]['source']['name'];?> </h5>
+  </div>
+</div>
+
+
+
+<div class="row">
+<div class= "leftColumn">
+    <h2 id='headline'><a href = <?php echo $data['articles'][8]["url"] ?> ><?php echo $data['articles'][8]["title"];?> </a></h2>
+    <p id='description'><?php echo $data['articles'][8]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][8]["publishedAt"];?> by <?php echo $data['articles'][8]['source']['name'];?> </h5>
+  </div>
+  <div class = "middleColumn">
+      
+      <div class="leftImgs">
+      <?php  echo "<a href=" . $data['articles'][8]['url'] . " ><img  src='" . $data['articles'][8]['urlToImage'] . "' alt='" . $data['articles'][8]["title"]. "'> </a>"; ?>
+      </div>
+  </div>
+
+  <div class= "rightColumn">
+  <h2 id ='headline'> <a href = <?php echo $data['articles'][5]["url"] ?> ><?php echo $data['articles'][5]["title"];?> </a></h2>
+  
+    <div class="rightImgs">
+    <?php  echo "<a href=" . $data['articles'][5]['url'] . " ><img  src='" . $data['articles'][5]['urlToImage'] . "' alt='" . $data['articles'][5]["title"]. "'> </a>"; ?>
+    </div>
+    <p id= 'description'><?php echo $data['articles'][5]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][5]["publishedAt"];?> by <?php echo $data['articles'][5]['source']['name'];?> </h5>
+  </div>
+</div>
+
+
+
+
+
+<div class="row">
+<div class= "leftColumn">
+    <h2 id='headline'><a href = <?php echo $data['articles'][6]["url"] ?> ><?php echo $data['articles'][6]["title"];?> </a></h2>
+    <p id='description'><?php echo $data['articles'][6]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][6]["publishedAt"];?> by <?php echo $data['articles'][6]['source']['name'];?> </h5>
+  </div>
+  <div class = "middleColumn">
+      
+      <div class="leftImgs">
+      <?php  echo "<a href=" . $data['articles'][6]['url'] . " ><img  src='" . $data['articles'][6]['urlToImage'] . "' alt='" . $data['articles'][0]["title"]. "'> </a>"; ?>
+      </div>
+  </div>
+
+  <div class= "rightColumn">
+  <h2 id ='headline'> <a href = <?php echo $data['articles'][7]["url"] ?> ><?php echo $data['articles'][7]["title"];?> </a></h2>
+  
+    <div class="rightImgs">
+    <?php  echo "<a href=" . $data['articles'][7]['url'] . " ><img  src='" . $data['articles'][7]['urlToImage'] . "' alt='" . $data['articles'][7]["title"]. "'> </a>"; ?>
+    </div>
+    <p id= 'description'><?php echo $data['articles'][7]["description"];?></p>
+    <h5 id='published'>Posted: <?php echo $data['articles'][7]["publishedAt"];?> by <?php echo $data['articles'][7]['source']['name'];?> </h5>
+  </div>
+</div>
+
  
 
   <footer>
-    <h2>FOOTER</h2>
+  <div class='leftElement'>
+     <h2>Thanks for Reading!</h2>
+    </div>
+
+  
+  
+  <div class=titleDiv>
+  <img class = 'logo' src="Resources/RU_SHIELD_BLACK.png">
+      <h1 class = 'title'> RUTGERS TIMES</h1>
+  </div>
+
+   <div class="rightElement">
+   <form action="subscription.php">
+         <button type="submit">Subscribe!</button>
+      </form>
+    </div>
+
   </footer>
 </body>
 </html>
